63b329beeaba63ef24b860ab7b90c39873d45c92
Zachary Duve <zacharyduve@Ganymede.local>
2025-02-15 00:20:11 -0500

    Adding some more tests

--- tree/BST_test.go
+++ tree/BST_test.go
@@ -6,12 +6,18 @@ import (
 	"github.com/ZacharyDuve/godatacollections"
 )
 
+func intBST(zeroVal int) *BST[int, int] {
+	bst, _ := NewBST(func(a, b int) int { return a - b }, func(a int) int { return a }, zeroVal)
+
+	return bst
+}
+
 func TestBSTImplementsSet(t *testing.T) {
-	var _ godatacollections.Set[string, string] = &BST[string, string]{}
+	var _ godatacollections.Set[int, int] = intBST(0)
 }
 
 func TestBSTStartsEmpty(t *testing.T) {
-	bst, _ := NewBST(func(a, b int) int { return a - b }, func(a int) int { return a }, 0)
+	bst := intBST(0)
 
 	if bst.root != nil {
 		t.Fail()
@@ -19,7 +25,7 @@ func TestBSTStartsEmpty(t *testing.T) {
 }
 
 func TestBSTAddingOneOnlyAddsOneItem(t *testing.T) {
-	bst, _ := NewBST(func(a, b int) int { return a - b }, func(a int) int { return a }, 0)
+	bst := intBST(0)
 
 	bst.Insert(5)
 
@@ -35,4 +41,49 @@ func TestBSTAddingOneOnlyAddsOneItem(t *testing.T) {
 		count++
 	}
 
+}
+
+func TestAddingIn3ValuesAndThenRemovingRootPromotesRightSideNode(t *testing.T) {
+	bst := intBST(0)
+
+	// First should be root
+	bst.Insert(5)
+	// Goes left
+	bst.Insert(1)
+	// Goes right
+	bst.Insert(10)
+
+	oldRoot := bst.root
+	// Removes our root
+	bst.Remove(oldRoot.key)
+
+	if bst.root != oldRoot.right {
+		t.Fail()
+	}
+}
+
+func TestRemovingWhenNotingHasBeenAddedReturnsError(t *testing.T) {
+	bst := intBST(0)
+
+	err := bst.Remove(1)
+
+	if err == nil {
+		t.Fail()
+	}
+}
+
+func TestAddingDuplicateReturnsError(t *testing.T) {
+	bst := intBST(0)
+
+	err := bst.Insert(1)
+
+	if err != nil {
+		t.Fail()
+	}
+
+	err = bst.Insert(1)
+
+	if err == nil {
+		t.Fail()
+	}
 }
